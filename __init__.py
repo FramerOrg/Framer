60b1a9e4eb50bf0f737ffb0d2908e011117a8733
runoneall <runoobsteve@hotmail.com>
2025-05-03 16:13:17 +0800

    update

---
+++ __init__.py
@@ -0,0 +1,48 @@
+def init(link_to=None, log_name="Framer", hook_error=False):
+
+    # python module import
+    import os
+    import sys
+    import types
+    import functools
+    import traceback
+
+    # local module import
+    from . import helper
+
+    # create framer namespace
+    framer = types.SimpleNamespace()
+    framer.helper = helper
+    framer.logger = functools.partial(framer.helper.logger, log_name)
+
+    # temporary logger for init
+    init_logger = functools.partial(framer.helper.logger, "Init")
+
+    # if enable error hook
+    if hook_error:
+        sys.excepthook = framer.helper.global_except_hook
+
+    try:
+
+        # load env
+        if os.path.exists("env.json"):
+            init_logger("Loading env.json...")
+            import json
+
+            with open("./env.json", "r", encoding="UTF-8") as f:
+                env = json.load(f)
+            framer.env = types.SimpleNamespace()
+            for key, value in env.items():
+                init_logger(f"Setting {key} to {value}...")
+                setattr(framer.env, key, value)
+
+    except:
+        init_logger(traceback.format_exc())
+
+    # return framer
+    if link_to is None:
+        return framer
+    else:
+        for attr in dir(framer):
+            if not attr.startswith("__"):
+                sys.modules[link_to].__dict__[attr] = getattr(framer, attr)
---
+++ helper.py
@@ -0,0 +1,56 @@
+def logger(from_module: str, message: str, max_width: int = None):
+    import time
+
+    def format_with_wrap(msg: str, width: int):
+        import textwrap
+
+        result = []
+        for line in msg.splitlines(keepends=True):
+            if len(line.strip()) == 0 or len(line) <= width:
+                result.append(line)
+            else:
+                wrapped_lines = textwrap.wrap(
+                    line, width=width, break_long_words=True, replace_whitespace=False
+                )
+                result.extend([l + "\n" for l in wrapped_lines])
+        return "".join(result)
+
+    def get_terminal_width():
+        import os
+
+        try:
+            columns = os.get_terminal_size().columns - 2
+            return columns
+        except (AttributeError, OSError):
+            return 80
+
+    if max_width is None:
+        max_width = get_terminal_width()
+
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    # if message only one line
+    if "\n" not in message:
+        print(f"* From {from_module} At {current_time}: {message}")
+        return
+
+    # if message multiple lines
+    if "\n" in message:
+        message = format_with_wrap(message, width=max_width)
+        print(f"* From {from_module} At {current_time}:")
+        print("|", "\n| ".join(message.split("\n")), "\n")
+        return
+
+
+def global_except_hook(exc_type, exc_value, exc_traceback):
+    import sys
+    import traceback
+
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger(
+        "ErrHooker",
+        "".join(traceback.format_exception(exc_type, exc_value, exc_traceback)),
+    )
